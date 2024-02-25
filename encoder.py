3c81e5fcdc93d7af782dafb085b360d04ddac6b7
Michal Zapala <zapalamichal1@gmail.com>
2024-02-25 15:58:10 +0100

    add encoder logic

---
+++ encoder.py
@@ -0,0 +1,32 @@
+from gf16_operations import generating_poly, gf16_poly_divide
+
+# Encoding function
+def rs_encode(data):
+
+    # Splitting data into 9-element blocks
+    block_size = 9
+    num_blocks = (len(data) - 1) // block_size + 1
+    blocks = [data[i:i+block_size] for i in range(0, len(data), block_size)]
+    
+    # Generating error correction symbols 
+    ecc_size = 6
+
+    ecc_blocks = []
+    for block in blocks:
+        block_poly = block
+        block_ecc_poly = block_poly + [0] * 6
+        block_ecc_poly =  gf16_poly_divide(block_ecc_poly, generating_poly)
+        ecc_blocks.append(block_ecc_poly[:ecc_size])
+        print("\nERROR CORRECTION PART:")
+        print(ecc_blocks)
+
+    # Combining data blocks with error correction symbols
+    final_blocks = []
+    for i in range(num_blocks):
+        data_block = blocks[i]
+        ecc_block = ecc_blocks[i]
+        final_block = data_block + ecc_block
+        final_blocks += final_block 
+    
+    # Returning final blocks
+    return final_blocks
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import tkinter as tk
 import ui_manager 
+import encoder
 
 def main():
+    data = [0, 0, 0, 0, 0, 0, 0, 0, 1]
+    encoded_data = encoder.rs_encode(data) 
+    print("\ENCODED DATA:")
+    print(encoded_data)
+
     root = tk.Tk()
     ui_manager.ui_config(root)
     root.mainloop()
+    
 
 if __name__ == "__main__":
     main()
